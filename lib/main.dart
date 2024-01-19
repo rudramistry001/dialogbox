797f0ff0fcf5969ac437680ddcebb22441e00aa0
rudramistry001 <rudramistry74@yahoo.com>
2024-01-19 16:54:35 +0530

    this is for downloading an file from  the url

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:dialogbox/home_screen.dart';
+import 'package:dialogbox/singledownload.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const SingleDownloadScreen(),
     );
   }
 }
